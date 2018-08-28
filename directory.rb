1c438f60880d6b396598ad006eb0df845ed37744
Amy Denny-Guerin <a.dennyguerin@gmail.com>
2018-08-28 10:32:01 +0000

    Refactored code further

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
-student_count = 11
-# adding a comment. Student names.
+# Now they're in an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates",
+]
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each { |student| puts student }
 # adding another comment. No. of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
