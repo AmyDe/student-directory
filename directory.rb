4c3a4f5d8e98d523b956650dbd6ada7ed331519d
Amy Denny-Guerin <a.dennyguerin@gmail.com>
2018-08-28 10:45:36 +0000

    Refactored code to use methods

--- directory.rb
+++ directory.rb
@@ -12,8 +12,17 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates",
 ]
-puts "The students of Villains Academy"
-puts "-------------"
-students.each { |student| puts student }
-# adding another comment. No. of students
-puts "Overall, we have #{students.count} great students"
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print_names(names)
+  names.each { |student| puts student }
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print_names(students)
+print_footer(students)
