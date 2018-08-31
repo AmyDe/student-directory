b3cecad9001e4a361dcbca4968f37714560ac46b
Amy Denny-Guerin <35347912+AmyDe@users.noreply.github.com>
2018-08-31 11:45:48 +0100

    Updated to make more descriptive - methods have fewer lines
    AWS C9 account ran out so updating on here for final couple of changes.

--- directory.rb
+++ directory.rb
@@ -1,22 +1,22 @@
+@students = []
+
 def input_students
   puts "Please enter the student's name:"
   name = gets.chomp
   puts "Please enter the cohort month:"
   puts "(To finish, hit return twice)"
-  students = []
   
   while !name.empty?
     cohort = gets.chomp
     cohort = "Forever" if cohort.empty?
-    students << {name: name, cohort: cohort}
-    if students.count != 1
-      puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort}
+    if @students.count != 1
+      puts "Now we have #{@students.count} students"
     else
-      puts "Now we have #{students.count} student"
+      puts "Now we have #{@students.count} student"
     end
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -24,57 +24,67 @@ def print_header
   puts "-------------".center(50)
 end
 
-def print_names(student_data)
+def print_names
+  
+stu_array = {}
 
-student_cohort = {}
-
-student_data.each { |stu|
+@students.each { |stu|
   cohort_group = stu[:cohort]
   
-    if student_cohort[cohort_group] == nil
-      student_cohort[cohort_group] = []
+    if stu_array[cohort_group] == nil
+      stu_array[cohort_group] = []
     end
-    student_cohort[cohort_group].push(stu[:name])
+    stu_array[cohort_group].push(stu[:name])
     }
 
-student_cohort.each { |cohort, name|
+stu_array.each { |cohort, name|
   puts "Cohort Month - #{cohort}:"
   puts name
   }
 
 end
 
-def print_footer(names)
-  if names.count != 1
-    puts "Overall, we have #{names.count} great students".center(50)
+def print_footer
+  if @students.count != 1
+    puts "Overall, we have #{@students.count} great students".center(50)
   else
     puts "Overall, we have 1 great student".center(50)
   end
 end
 
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  if !@students.empty?
+    print_header
+    print_names
+    print_footer
+  else
+    puts "You need to enter student data"
+  end
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      if !students.empty?
-        print_header
-        print_names(students)
-        print_footer(students)
-      else
-        puts "You need to enter student data"
-      end
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you meant, try again"
-    end
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
